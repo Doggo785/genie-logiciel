b7b91ef0745e0f327a1275667f2d83dab91550d2
Doggo785 <stephane.plathey@proton.me>
2026-02-06 09:51:29 +0100

    Add GestionMemoire project and delegate example
    Added GestionMemoire project to the solution with a .NET 10.0 executable configuration. Introduced Delegate.cs to demonstrate delegate usage and callback handling in C#.

---
+++ GestionMemoire/Delegate.cs
@@ -0,0 +1,17 @@
+﻿int methode(int a, int b)
+{
+        return a + b;
+}
+
+Callback handler = methode;
+
+static void MethodWithCallback(int a, int b, Callback callback)
+{
+    Console.WriteLine("The result : " + callback(a, b) );
+}
+
+MethodWithCallback(5, 10, handler);
+
+delegate int Callback(int a, int b);
+
+
