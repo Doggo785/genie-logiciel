580471464e14579807e83140cff62c20c52480ed
Doggo785 <stephane.plathey@proton.me>
2026-02-10 09:24:53 +0100

    Add GestionDesProcessus project for process management
    Created new GestionDesProcessus console app targeting .NET 10.0. Added Program.cs with methods to launch and manage external processes (e.g., explorer.exe), including error handling and demonstration code. Updated solution file to include the new project.

---
+++ GestionDesProcessus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Xml.Serialization;
+
+namespace GestionDesProcessus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //LancerProcessus("notepad.exe");
+        //LancerProcessus("explorer.exe");
+        Process processus = new Process();
+        processus.StartInfo.FileName = "explorer.exe";
+        processus.StartInfo.Arguments = @"C:\Windows";
+        processus.Start();
+        Thread.Sleep(2000);
+        processus.WaitForExit();
+
+        //Console.ReadKey();
+    }
+    static void LancerProcessus(string nomProcessus)
+    {
+        Process processus = new Process();
+        processus.StartInfo.FileName = nomProcessus;
+
+
+        try
+        {
+            bool Launched = processus.Start();
+            if (Launched)
+            {
+                Console.WriteLine($"Processus '{nomProcessus}' lancé avec succès.");
+                processus.WaitForExit();
+            }
+            else
+            {
+                Console.WriteLine($"Échec du lancement du processus '{nomProcessus}'.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur lors du lancement du processus: {ex.Message}");
+        }
+    }
+}
